40e2d7d9541cee8e6c40a80ff7be193ca690f4f7
Steven Baty <baty_steven@yahoo.com>
2021-04-07 01:14:02 -0600

    updated routes for category/ findALL, findOne, post, put, updated, delete

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const routes = require('./routes');
 // import sequelize connection
 
@@ -7,19 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
