9440923ab6fd7a257071f32e4e3c603ba9221440
Steven Baty <baty_steven@yahoo.com>
2021-04-07 01:37:12 -0600

    updated product routes get all, get one, post, delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,7 +46,7 @@ router.get('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCategoryData);
@@ -82,7 +82,7 @@ router.put('/:id', (req, res) => {
   )
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCategoryData);
@@ -102,7 +102,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCategoryData);
